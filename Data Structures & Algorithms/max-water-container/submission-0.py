86891505f7dcd7d423a72cb48c370eb66e718c26
Mohnishmxh <mohnish.ranganeni@gmail.com>
2026-09-10 20:36:23 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        max_water = 0
+
+        while left < right:
+            # Area is width multiplied by the shorter of the two bars
+            width = right - left
+            current_water = width * min(height[left], height[right])
+            max_water = max(max_water, current_water)
+
+            # Move the pointer with the smaller height inward
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
